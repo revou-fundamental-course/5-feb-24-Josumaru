ca83011266e1e0d49f38fb8447f0e560804127be
Josumaru <jokosupriyanto1712@gmail.com>
2024-02-10 09:51:39 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const converter = document.getElementsByClassName("coverter");
+
+isCelcius = true;
+
+const setConverter = (page) => {
+    scrollTo({
+        left:0,
+        top: window.innerWidth*page, 
+        behavior: "smooth"
+    })
+}
+
+
+const reverse = () => {
+    isCelcius = !isCelcius;
+    document.getElementById("input-status").textContent = isCelcius ? "celcius@archlinux" : "fahrenheit@archlinux"
+    document.getElementById("output-status").textContent = !isCelcius ? "celcius@archlinux" : "fahrenheit@archlinux"
+    tmp = document.getElementById("output").value
+    document.getElementById("output").value = document.getElementById("input").value
+    document.getElementById("input").value = tmp
+}
+
+const convert = (element) => {
+    const data = element.value
+    const value = data.split("#")[1]
+    // console.log(data, data.split('#').length)
+    document.getElementById("output").value = isCelcius ? `[fahrenheit@archlinux ~]#${(value*9/5)+32}` : `[celcius@archlinux ~]#${(value - 32)*5/9}`;
+    
+    if (data.split('#').length == 1) {
+        reset()
+    }
+}
+
+const reset = () => {
+    document.getElementById('input').value = isCelcius ?  "[celcius@archlinux ~]#" : "[fahrenheit@archlinux ~]#";
+    document.getElementById('output').value = !isCelcius ? "[celcius@archlinux ~]#" : "[fahrenheit@archlinux ~]#";
+}
